ffc83c6fa9fd91dcda554b15fb2ea4f338669c56
OmarH260 <144048142+OmarH260@users.noreply.github.com>
2023-11-01 20:31:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/wishit/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.wishit;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.wishit.AddDataFire.AddRestaurantFragment;
+import com.example.wishit.SingUpLogin.ForgotFragment;
+import com.example.wishit.SingUpLogin.LoginFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gotoAddProduct();
+        //gotoLoginFragment();
+    }
+
+    private void gotoLoginFragment(){
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.frameLayoutMain, new LoginFragment());
+        ft.commit();
+    }
+
+    private void gotoAddProduct(){
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.frameLayoutMain, new AddRestaurantFragment());
+    }
+}
